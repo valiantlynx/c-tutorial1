870ca75091f15b84845a74c4c550f309688302c8
Gormery Kombo Wanjiru <gormerykw@uia.no>
2023-09-05 20:09:57 +0200

    initial commit from clion

---
+++ for_loops.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+
+    while (i <= 5 ){
+        printf("%i: \n", i);
+        i++;
+    }
+    return 0;
+}
--- game_2_0.c
+++ game_2_0.c
@@ -2,6 +2,27 @@
 
 int main()
 {
+    int secretNumber = 5;
+    int guess = 0;
+    int guessCount = 0;
+    int guessLimit = 3;
+    int ourOfGuesses = 0;
+
+    while ( guess != secretNumber && ourOfGuesses == 0 ){
+        if (guessCount < guessLimit) {
+            printf("Enter secret number: \n");
+            scanf("%i", &guess);
+            guessCount++;
+        } else {
+            ourOfGuesses = 1;
+        }
+    }
+
+    if (ourOfGuesses == 0) {
+        printf("You win!\n");
+    } else {
+        printf("Out of gueses!\n");
+    }
 
     return 0;
 }
